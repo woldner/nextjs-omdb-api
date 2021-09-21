718b31cae881913f4ad6a73b63af36ec17d0ecdf
Jean Alexander Woldner <alexander@woldner.io>
2021-09-21 17:55:17 +0200

    :wrench: update next config with custom alias @

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
+const { resolve } = require("path")
+
 module.exports = {
   reactStrictMode: true,
+
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": resolve(__dirname),
+    }
+
+    return config
+  },
 }
