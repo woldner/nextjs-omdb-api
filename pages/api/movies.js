5ad3724261657ec3702442da99d536a944aa7f9e
Jean Alexander Woldner <alexander@woldner.io>
2021-09-21 17:24:48 +0200

    :truck: replace api/hello with api/movies

---
+++ pages/api/movies.js
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+const { API_KEY } = process.env;
+
+export default function handler(req, res) {
+  console.log("api key =>", API_KEY);
+  res.status(200).json({ name: "John Doe" });
+}
