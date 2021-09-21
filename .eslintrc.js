00cfe0d9dac44a8d9db215ad2f0a542fc95213c3
Jean Alexander Woldner <alexander@woldner.io>
2021-09-21 17:43:35 +0200

    :heavy_plus_sign: add prettier

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
+    "next/core-web-vitals",
+    "plugin:prettier/recommended",
+  ],
+  root: true,
+  rules: {
+    "prettier/prettier": ["error", {}, { usePrettierrc: true }],
+    "no-unused-vars": ["warn", { argsIgnorePattern: "^_", ignoreRestSiblings: true }],
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": ["error", { ignore: ["children", "Component", "pageProps"] }],
+  },
+}
