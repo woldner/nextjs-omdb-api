5c3ea8e655e3d5daf9c9c5f8fc9cde751d0241bc
Jean Alexander Woldner <alexander@woldner.io>
2021-09-22 10:13:13 +0200

    :recycle: remove irrelevant code; move head element

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
+import Head from "next/head"
 import "@/styles/globals.css"
 
 const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Next.js OMDb api</title>
+        <meta name="description" content="Next.js/OMDb api playground" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default App
