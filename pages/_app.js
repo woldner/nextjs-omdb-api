416d93f1abda7dabc19e28cab157dabc1a131212
Jean Alexander Woldner <alexander@woldner.io>
2021-09-22 17:00:14 +0200

    :sparkles: use mui with next

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,30 @@
 import Head from "next/head"
+import { ThemeProvider } from "@mui/material/styles"
+import CssBaseline from "@mui/material/CssBaseline"
+import { CacheProvider } from "@emotion/react"
+import theme from "@/src/theme"
+import createEmotionCache from "@/src/cache"
 import "@/styles/globals.css"
 
-const App = ({ Component, pageProps }) => {
+const cache = createEmotionCache()
+
+const App = ({ Component, emotionCache = cache, pageProps }) => {
   return (
-    <>
+    <CacheProvider value={emotionCache}>
       <Head>
         <title>Next.js OMDb api</title>
+
         <meta name="description" content="Next.js/OMDb api playground" />
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Component {...pageProps} />
-    </>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
   )
 }
 
