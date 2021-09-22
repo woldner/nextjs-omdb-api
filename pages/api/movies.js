adb130e361162220772d4c3d24c4a3a0d01c59cd
Jean Alexander Woldner <alexander@woldner.io>
2021-09-22 10:14:29 +0200

    :sparkles: omdb movie api integration

--- pages/api/movies.js
+++ pages/api/movies.js
@@ -1,6 +1,19 @@
 const { API_KEY } = process.env
 
-export default function handler(req, res) {
-  console.log("api key =>", API_KEY)
-  res.status(200).json({ name: "John Doe" })
+const handler = async ({ body }, res) => {
+  const { title } = body
+
+  try {
+    const response = await fetch(`http://omdbapi.com/?apikey=${API_KEY}&t=${title}`)
+    const result = await response.json()
+
+    res.status(200).json(result)
+  } catch (error) {
+    console.error("unable to fetch movies")
+    console.error(error)
+
+    return res.status(500)
+  }
 }
+
+export default handler
